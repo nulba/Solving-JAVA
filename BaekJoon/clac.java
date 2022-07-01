5ecf057aa4d97440bd24671af562d2899c4b05b7
nulba <skflsmfqh@gmail.com>
2022-07-01 21:50:56 +0900

    220701

---
+++ BaekJoon/clac.java
@@ -0,0 +1,23 @@
+package my;
+
+import java.util.Scanner;
+
+public class Calc {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		sc.close();
+		
+		System.out.println(A + B);
+		System.out.println(A - B);
+		System.out.println(A * B);
+		System.out.println(A / B);
+		System.out.println(A % B);
+		
+
+	}
+
+}
