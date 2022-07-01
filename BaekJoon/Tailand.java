b0700d5cffe11934e0713572dd950375a1991d5b
nulba <skflsmfqh@gmail.com>
2022-07-01 22:01:06 +0900

    220701

---
+++ BaekJoon/Tailand.java
@@ -0,0 +1,19 @@
+package my;
+
+import java.util.Scanner;
+
+public class Tailand {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int y = sc.nextInt();
+		sc.close();
+		
+		int x = 543;
+		
+		System.out.println(y - x);
+
+	}
+
+}
